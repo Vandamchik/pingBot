4dd3160ee3ba617a33979d8da5fb98505ef13f8b
Vandamchik <cherepcock@gmail.com>
2025-07-03 17:02:42 +0300

    setting backend logic, add webhooks for telegram bot

--- Backend/index.js
+++ Backend/index.js
@@ -2,15 +2,17 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express')
 const cors = require('cors')
 
-const { BOT_TOKEN, PORT } = require('./src/config/env');
+const { BOT_TOKEN, PORT, FRONTEND_URL } = require('./src/config/env');
 const { handleMessage } = require('./src/handlers/messageHandler');
 const routes = require('./src/routes');
 const connectDB = require('./src/config/db');
 const errorHandler = require('./src/middlewares/errorHandler');
 
 
-const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 const app = express();
+const bot = new TelegramBot(BOT_TOKEN);
+
+const WEBHOOK_URL = `${FRONTEND_URL}/bot${BOT_TOKEN}`;
 
 app.use(express.json());
 app.use(cors())
@@ -24,10 +26,15 @@ app.use(errorHandler);
 (async () => {
     try {
       await connectDB();
-      app.listen(PORT, () => console.log(`~~ Server running on PORT: ${PORT} ~~`));
+      await bot.setWebHook(WEBHOOK_URL);
+      app.listen(PORT, () => {
+        console.log(`✅ Server running on port ${PORT}`);
+        console.log(`✅ Webhook set: ${WEBHOOK_URL}`);
+      });
     } catch (err) {
-      console.error('~~ Error on runnig Server ~~', err.message);
+      console.error('❌ Error starting server:', err.message);
       process.exit(1);
     }
   })();
+  
   
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const pingRoutes = require('./ping/pingRoutes');
+const botRoutes = require('./bot/botRoutes');
+const { BOT_TOKEN } = require('../config/env')
 
 module.exports = (bot) => {
   const router = express.Router();
 
+  router.post(`/bot/${BOT_TOKEN}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
   router.use('/ping', pingRoutes(bot));
 
   return router;
